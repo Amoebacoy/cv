0373667bd65e9d263ea4c1bee1c388b3fee6119f
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-18 18:04:32 +0700

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -39,7 +39,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://${akbarvpn}/vpn.zip
+wget https://raw.githubusercontent.com/Amoebacoy/cv/main/vpn/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
