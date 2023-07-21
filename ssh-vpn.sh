6533abe954e34357d0fe1978207fc2eaab90e251
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-21 07:57:47 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -190,13 +190,13 @@ mkdir -p /home/vps/public_html
 # install badvpn
 cd
 cd
-wget -O /usr/bin/badvpn-udpgw http://sgpx.cybervpn.site:81/Autoscript-by-azi-main/udpgw/badvpn-udpgw && chmod +x  /usr/bin/badvpn-udpgw
+wget -O /usr/bin/badvpn-udpgw https://raw.githubusercontent.com/Amoebacoy/cv/main/ssh-vpn/badvpn-udpgw && chmod +x  /usr/bin/badvpn-udpgw
 #system badvpn 7300
-wget -O /etc/systemd/system/svr-7300.service http://sgpx.cybervpn.site:81/Autoscript-by-azi-main/udpgw/svr-7300.service && chmod +x  /etc/systemd/system/svr-7300.service
+wget -O /etc/systemd/system/svr-7300.service https://raw.githubusercontent.com/Amoebacoy/cv/main/ssh-vpn/svr-7300.service && chmod +x  /etc/systemd/system/svr-7300.service
 #system badvpn 7200
-wget -O /etc/systemd/system/svr-7200.service http://sgpx.cybervpn.site:81/Autoscript-by-azi-main/udpgw/svr-7200.service && chmod +x  /etc/systemd/system/svr-7200.service
+wget -O /etc/systemd/system/svr-7200.service https://raw.githubusercontent.com/Amoebacoy/cv/main/ssh-vpn/svr-7200.service && chmod +x  /etc/systemd/system/svr-7200.service
 #system badvpn 7100
-wget -O /etc/systemd/system/svr-7100.service http://sgpx.cybervpn.site:81/Autoscript-by-azi-main/udpgw/svr-7100.service && chmod +x  /etc/systemd/system/svr-7100.service
+wget -O /etc/systemd/system/svr-7100.service https://raw.githubusercontent.com/Amoebacoy/cv/main/ssh-vpn/svr-7100.service && chmod +x  /etc/systemd/system/svr-7100.service
 
 #reboot system 7100
 systemctl daemon-reload
@@ -277,6 +277,8 @@ cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
 /etc/init.d/stunnel4 restart
 
+#installer OPH
+wget https://raw.githubusercontent.com/Amoebacoy/cv/main/ohp/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 
 # install fail2ban
 apt -y install fail2ban
@@ -374,27 +376,35 @@ fi
 # finishing
 cd
 chown -R www-data:www-data /home/vps/public_html
-sleep 1
+sleep 0.5
 echo -e "$yell[SERVICE]$NC Restart All service SSH & OVPN"
 /etc/init.d/nginx restart >/dev/null 2>&1
-sleep 1
-echo -e "[ ${green}ok${NC} ] Restarting nginx"
+sleep 0.5
+echo -e "[ ${green}ok${NC} ] Restarting nginx "
 /etc/init.d/openvpn restart >/dev/null 2>&1
-sleep 1
-echo -e "[ ${green}ok${NC} ] Restarting cron "
+sleep 0.5
+echo -e "[ ${green}ok${NC} ] Restarting openvpn "
 /etc/init.d/ssh restart >/dev/null 2>&1
-sleep 1
+sleep 0.5
 echo -e "[ ${green}ok${NC} ] Restarting ssh "
 /etc/init.d/dropbear restart >/dev/null 2>&1
-sleep 1
+sleep 0.5
 echo -e "[ ${green}ok${NC} ] Restarting dropbear "
 /etc/init.d/fail2ban restart >/dev/null 2>&1
-sleep 1
+sleep 0.5
 echo -e "[ ${green}ok${NC} ] Restarting fail2ban "
 /etc/init.d/stunnel4 restart >/dev/null 2>&1
-sleep 1
+sleep 0.5
 echo -e "[ ${green}ok${NC} ] Restarting stunnel4 "
 /etc/init.d/vnstat restart >/dev/null 2>&1
+sleep 0.5
+echo -e "[ ${green}ok${NC} ] Restarting vnstat "
+/etc/init.d/squid restart >/dev/null 2>&1
+sleep 0.5
+echo -e "[ ${green}ok${NC} ] Restarting squid "
+/etc/init.d/cron restart >/dev/null 2>&1
+sleep 0.5
+echo -e "[ ${green}ok${NC} ] Restarting cron "
 sleep 1
 
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
@@ -411,14 +421,13 @@ echo "unset HISTFILE" >> /etc/profile
 # finihsing
 clear
 
-#installer OPH
-wget https://gitlab.com/hidessh/baru/-/raw/main/ohp.sh && chmod +x ohp.sh && ./ohp.sh
-#installer openvpn
 
 rm -f /root/key.pem
 rm -f /root/cert.pem
 rm -f /root/ssh-vpn.sh
 rm -f /root/bbr.sh
+rm -f /root/ohp.sh
+rm -f /root/set-br.sh
 
 # finihsing
 clear
