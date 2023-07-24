58712f35104c7ffe2f2bf2ca6c943e102f46e274
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-24 13:15:31 +0700

    Update pelanggar.sh

--- senmenu/pelanggar.sh
+++ senmenu/pelanggar.sh
@@ -3,5 +3,5 @@ figlet -f 3d "User yang melanggar" | lolcat
 cat log-limit.txt
 
 echo "==============================================" | lolcat
-echo "                 CyberVPN TUNNELING"
+echo "                 Aby&Fabumi TUNNELING"
 echo "==============================================" | lolcat
